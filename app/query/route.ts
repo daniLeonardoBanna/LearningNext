8f341a821c72a2b1ce6dd3d25f27423222d3c363
daniLeonardoBanna <danileonardoelbanna@gmail.com>
2025-12-13 15:17:28 +0200

    finished chapter 9 or file grouping and next streaming

--- app/query/route.ts
+++ app/query/route.ts
@@ -19,7 +19,9 @@ export async function GET() {
   //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
   // });
   try {
-    return Response.json(await listInvoices());
+    const queryResult = await listInvoices();
+
+    return Response.json(queryResult);
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
